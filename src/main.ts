b83960888cccbc13da03b9fa8f1bb5e2b65595d3
maths <matheusalan.dev@gmail.com>
2026-01-28 10:50:31 -0300

    Configuracao inicial do projeto. Pronto para iniciar implementacoes.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sistema Delivery')
+    .setDescription('Projeto Delivery')
+    .setContact(
+      "Grupo 3",
+      "http://www.link-do-deploy.com",
+      "g3gen@gmail.com")
+    .setVersion('1.0')
+    // .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '-03:00'
 
@@ -12,6 +26,6 @@ async function bootstrap() {
   app.enableCors();
 
   await app.listen(process.env.PORT ?? 4000);
-  
+
 }
 bootstrap();
