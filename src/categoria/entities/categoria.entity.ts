21773855815cf1912b515c5ddc53f36169c1327c
maths <matheusalan.dev@gmail.com>
2026-01-28 19:29:01 -0300

    Adicionando decorators do swagger nas camadas entity e controller

--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, MinLength } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 /*import { Produto } from "../../produto/entities/produto.entity";*/
@@ -7,11 +8,13 @@ export class Categoria {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @IsNotEmpty()
   @MinLength(3)
   @Column({ length: 255, nullable: false })
   descricao: string;
 
+  //@ApiProperty()
   /*@OneToMany(() => Produto, (produto) => produto.categoria)
   produtos: Produto[];*/
 }
